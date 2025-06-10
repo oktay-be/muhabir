08de065723ef57d249162a3021029f61145ce121
oktay-be <oktay.burak.ertas@gmail.com>
2025-06-10 18:56:49 +0200

    tests are implemented

--- test_clean_fix.py
+++
@@ -1,7 +0,0 @@
-from capabilities.scraping.extractors.ldjson_extractor import LdJsonExtractor
-
-extractor = LdJsonExtractor()
-print('Test 1:', repr(extractor._clean_json_string(' { "key" : "value" ')))
-print('Test 2:', repr(extractor._clean_json_string(' "key" : "value" } ')))
-print('Test 3:', repr(extractor._clean_json_string('invalid json')))
-print('Test 4:', repr(extractor._clean_json_string('{"key": "value"}')))
